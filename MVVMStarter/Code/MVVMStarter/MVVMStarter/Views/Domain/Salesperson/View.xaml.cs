13b2eda4e4348f7be4f2efd8397a4f1f06875b53
Nikolaj <nikodyring@gmail.com>
2017-05-16 12:21:45 +0200

    Rettet SalesPerson til Salesperson -> I hele solutionen.

--- MVVMStarter/Code/MVVMStarter/MVVMStarter/Views/Domain/Salesperson/View.xaml.cs
+++ MVVMStarter/Code/MVVMStarter/MVVMStarter/Views/Domain/Salesperson/View.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace MVVMStarter.Views.Domain.SalesPerson
+namespace MVVMStarter.Views.Domain.Salesperson
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
